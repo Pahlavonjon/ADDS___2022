61bf760874dd625928c438700659e9fc8fa0ba5b
Pahlavonjon <pahlavonjon.odilov@student.adelaide.edu.au>
2022-09-12 15:26:22 +0930

    1

--- makefile
+++
@@ -1,3 +0,0 @@
-compile:
-	g++ -o main.out -std=c++11 -O2 -Wall *.cpp
-	./main.out
---
+++ rec1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int factorial(int value,int n){
+    if (n <= 0)
+        return value;
+    return factorial(value*(n),n-1);
+}
+int main(void) {
+    cout << "\n "<<factorial(1,19)<<"\n";
+}
