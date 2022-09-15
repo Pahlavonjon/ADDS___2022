60495edc8e6d4d7360bf50bba30838421fec54d9
Pahlavonjon <pahlavonjon.odilov@student.adelaide.edu.au>
2022-09-15 13:27:20 +0930

    added

--- tail.cpp
+++ tail.cpp
@@ -13,7 +13,7 @@ int numDup(int arr[], int start, int size, int element) {
 }
 int main(void){
     int array[] = {1,2,4,4,5,6,7,4,4,10};
-    cout << "\n\n"<<sizeof(array)/sizeof(array[0])<<"\n\n";
+   // cout << "\n\n"<<sizeof(array)/sizeof(array[0])<<"\n\n";
     cout << "\n\n "<<numDup(array,0,0,4)<<"\n\n";
     return 0;
 }
