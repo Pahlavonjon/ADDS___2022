def2baf1ba216c5d397ca96fad9e6751dfe7942f
Pahlavonjon <pahlavonjon.odilov@student.adelaide.edu.au>
2022-09-15 13:38:58 +0930

    added

--- tail.cpp
+++ tail.cpp
@@ -1,9 +1,10 @@
 #include <iostream> // finds how many duplicates there are in the array
 using namespace std;
 int numDup(int arr[], int start, int size, int element) {
-    int dup = size;
-    int length = sizeof(arr)/sizeof(arr[0]);
-    if (start >= length){
+    int dup = 0;
+    dup += size;
+    int len = *(&arr + 1) - arr;
+    if (size >= len){
         return 0;
     }
     if (arr[start] == element) {
@@ -13,7 +14,6 @@ int numDup(int arr[], int start, int size, int element) {
 }
 int main(void){
     int array[] = {1,2,4,4,5,6,7,4,4,10};
-   // cout << "\n\n"<<sizeof(array)/sizeof(array[0])<<"\n\n";
     cout << "\n\n "<<numDup(array,0,0,4)<<"\n\n";
     return 0;
 }
