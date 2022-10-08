869db2c84cd0460851a5f7a4f5d193f91c9b0a38
Pahlavonjon <pahlavonjon.odilov@student.adelaide.edu.au>
2022-10-08 12:38:43 +1030

    finished

--- QuickSort.cpp
+++ QuickSort.cpp
@@ -3,60 +3,38 @@
 #include <bits/stdc++.h>
 #include <vector>
 using namespace std;
-QuickSort::QuickSort(){this->smaller_index = -1;this->recursion_index = 0; this->low = 0; this->high = 0; this->start = 0;}
+QuickSort::QuickSort(){this->low = 0; this->high = 0; this->start = 0;}
 
-// vector<int> QuickSort::sort(vector<int> list){
-//     if (list.size() == 1)
-//         return list;
-//     if (list.size() == 2){
-//         if (list.at(0) > list.at(1))
-//             swap(list.at(0),list.at(1));
-//             return list;
-//     }
-//     int pivot_value = list.at(2);
-//     int larger_pointer = 0;
-//     int smaller_pointer = 0;
-//     for (unsigned int k = 0; k < list.size(); k++){
-//         if (list.at(k) > pivot_value)
-//             smaller_pointer = k;
-//         if (list.at(k) < pivot_value)
-//             larger_pointer = k;
-//         if ((list.at(smaller_pointer) > list.at(larger_pointer)))
-//             swap(list.at(smaller_pointer),list.at(larger_pointer));
-//     }
-// }
-int QuickSort::helper(std::vector<int> list, int low, int high, int pivot){
-	int i = low;
-	int j = low;
-	while( i <= high){
-		if(list.at(i) > pivot){
-			i++;
-		}
-		else{
-			swap(list,i,j);
-			i++;
-			j++;
-		}
-	}
-	return j-1;
-}
-
-std::vector<int> QuickSort::sort(std::vector<int> list){
-    if (start = 0){
-        this->low = 0;
-        this->high = list.size()-1;
-        this->start = 1;
+    std::vector<int> QuickSort::sort(std::vector<int> list){
+        int pivot_value = 0;
+        int pivot_index = 2;
+        if (list.size() == 0)
+            return list;
+        if (list.size() == 1){
+            sorted_vector.push_back(list.at(0));
+            return sorted_vector;
+        }
+        if (list.size() == 2){
+            pivot_value = list.at(0);
+            if (list.at(0) > list.at(1)){
+                swap(list.at(0),list.at(1));
+            }
+            sorted_vector.push_back(list.at(0));
+            sorted_vector.push_back(list.at(1));
+            return sorted_vector;
+        }
+        std::vector<int> smaller_than_pivot;
+        std::vector<int> bigger_than_pivot;
+        pivot_value = list.at(pivot_index);
+        list.erase(list.begin()+pivot_index);
+        for (unsigned int k = 0; k < list.size(); k++){
+            if (list.at(k) > pivot_value)
+                bigger_than_pivot.push_back(list.at(k));
+            else if (list.at(k) < pivot_value)
+                smaller_than_pivot.push_back(list.at(k)); 
+        }
+        sort(smaller_than_pivot); // recursion
+        sorted_vector.push_back(pivot_value);
+        sort(bigger_than_pivot); // recursion
+        return sorted_vector;
     }
-	if (low < high){
-        int pivot = list.at(high);
-        int pos = helper(list, low, high, pivot);
-        low_b = low;
-        high_b = high;
-        this->high = pos-1;
-            sort(list, low, pos-1); // list, low, pos-1)
-        this-> low = pos+1;
-        this-> high = high_b;
-            sort(list, pos+1, high); // list, pos+1, high
-	}
-    return list;
-}
--- main.cpp
+++ main.cpp
@@ -6,30 +6,44 @@
 #include "RecursiveBinarySearch.h"
 using namespace std;
 
-int main(void){
-  //  vector<int> List_1 = {1,5,7,4,3,7,8,5,4,3,9,324,6576,34};
-   // vector<int> List_1 = {1,5,4,3,9,324,6576,34};
-    vector<int> List_1 = {4,3,2,1};
+void tester(){
+//  vector<int> List_1 = {1,5,7,4,3,7,8,5,4,3,9,324,6576,34};
+    vector<int> List_1 = {1,5,4,3,9,324,6576,34};
+   // vector<int> List_1 = {4,3,2,1};
     BubbleSort BubbleSorter;
     QuickSort QuickSorter;
-   // List_1 = BubbleSorter.sort(List_1);
+  //  List_1 = BubbleSorter.sort(List_1);
    // List_1.erase(List_1.begin(),List_1.begin()+1);
     List_1 = QuickSorter.sort(List_1);
-    cout << "\n\n";
+    cout << "\n\n  MAIN   \n\n";
     for (unsigned int k = 0; k < List_1.size(); k++){
         cout << " " << List_1.at(k);
     }
     cout << "\n\n";
     RecursiveBinarySearch Sucher;
-    cout << "  " << Sucher.search(List_1,1)<< "\n\n";
-    cout << "  " << Sucher.search(List_1,3)<< "\n\n";
-    cout << "  " << Sucher.search(List_1,4)<< "\n\n";
-    cout << "  " << Sucher.search(List_1,5)<< "\n\n";
-    cout << "  " << Sucher.search(List_1,9)<< "\n\n";
-    cout << "  " << Sucher.search(List_1,34)<< "\n\n";
-    cout << "  " << Sucher.search(List_1,324)<< "\n\n";
-    cout << "  " << Sucher.search(List_1,6576)<< "\n\n";
-
+   
     cout << "\n\n";
+
+}
+
+int main(void){
+    BubbleSort BubbleSorter;
+    QuickSort QuickSorter;
+    RecursiveBinarySearch Sucher;
+    int length = 8;
+    int array[length];
+    vector<int> the_list;
+    cin >> array[0] >> array[1] >> array[2] >> array[3] >>  array[4] >> array[5] >> array[6] >> array[7];
+    for (int k = 0; k < 8; k++){
+      the_list.push_back(array[k]);
+    }
+    the_list = QuickSorter.sort(the_list);
+    bool result = Sucher.search(the_list,1);
+    string Result = "";
+    if (result == false)
+        Result = "false";
+    else if (result == true)
+        Result = "true";
+    cout << Result << " " << the_list.at(0) << " " << the_list.at(1) << " " << the_list.at(2) << " " << the_list.at(3) << " " << the_list.at(4) << " " << the_list.at(5) << " " << the_list.at(6) << " " << the_list.at(7)<<"\n"; 
     return 0;
 }
