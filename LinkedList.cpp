9fa6b45893f6764ed1544bbfa1f0fef4ceff7bf8
Pahlavonjon <pahlavonjon.odilov@student.adelaide.edu.au>
2022-10-18 00:18:29 +1030

    finished tuesday morning

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -7,15 +7,27 @@ using namespace std;
         LinkedList::LinkedList(int* array_integers, int length_array){
           head = nullptr;
             for (int k = 0; k < length_array; k++){
-                // Node* next_node = new Node;
-                // next_node->set_data(array_integers[k]);
-                // next_node->set_pointer(head);
-                // head = next_node;
                 addEnd(array_integers[k]);
             }
         }
         LinkedList::~LinkedList(){
-            // delete!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            // if (head == nullptr)
+            //     return;
+            // Node *temporary = head;
+            // int size = 1;
+            // while (temporary->get_pointer() != nullptr){
+            //     temporary = temporary->get_pointer();
+            //     size++;
+            // }
+            // vector<Node*> pointers = new *Node[size];
+            // size = 0;
+            // temporary = head;
+            // while (temporary->get_pointer() != nullptr){
+            //     pointer.at(size) = temporary->get_pointer();
+            //     temporary = temporary->get_pointer();
+            // }
+            // delete pointers;
+
         }
         void LinkedList::addFront(int newItem){
             Node* next_node = new Node;
@@ -26,7 +38,6 @@ using namespace std;
         void LinkedList::addEnd(int newItem){
             Node* new_Node = new Node;
             new_Node->set_data(newItem);
-
             if (head == NULL){
                 head = new_Node;
                 return;
@@ -36,48 +47,20 @@ using namespace std;
                 temporary = temporary->get_pointer();
             }
             temporary->set_pointer(new_Node);
-
         }
         void LinkedList::addAtPosition(int position, int newItem){
-    
-            Node *temporary = head;
-            int size_of_list = 0;
-            while (temporary->get_pointer() != nullptr){
-                temporary = temporary->get_pointer();
-                size_of_list++;
-            }
-            if (position > size_of_list){
-                addEnd(newItem);
-                return;
-            }
-            if (position <= 1 || size_of_list <= 1){
+            Node* new_Node_here = new Node;
+            new_Node_here->set_data(newItem);
+            if (position <= 1){
                 addFront(newItem);
                 return;
             }
-            temporary = head; // this holds the node which is to be moved up or down
-            Node* second_temporary;
-            int re_indexing = 0;
-            while (temporary->get_pointer() != nullptr){
-                second_temporary = temporary;
-                temporary = temporary->get_pointer();
-                re_indexing++;
-                cout << " re_indexing " << re_indexing<< "   " << position <<"\n";
-                if (position == re_indexing){
-                    Node* new_Node_here = new Node;
-                    new_Node_here->set_data(newItem);
-                  //  second_temporary->get_pointer() = new_Node_here;
-                    new_Node_here->set_pointer(second_temporary->get_pointer());
-
-                }
-                    
-                if (re_indexing == position-1){
-                    Node* new_Node_here = new Node;
-                    new_Node_here->set_data(newItem);
-                    new_Node_here->set_pointer(temporary->get_pointer());
-                    temporary->set_pointer(new_Node_here);
-                    return;
-                }
+            Node* second_temporary = head;
+            for(int i=0; i<position-2; i++){
+                second_temporary=second_temporary->get_pointer();
             }
+            new_Node_here->set_pointer(second_temporary->get_pointer());
+            second_temporary->set_pointer(new_Node_here);
         }
         int LinkedList::search(int item){
             int position = 0;
@@ -103,17 +86,14 @@ using namespace std;
             while (temporary->get_pointer() != nullptr){
                 temporary = temporary->get_pointer();
                 length++;
-            //    cout << "delete end " << length <<"\n";
             }
             temporary = head;
-            
             if (length == 1){
                 delete temporary->get_pointer();
                 temporary->set_pointer(nullptr);
                 return;
             }
             int end = length-1;
-          //  cout << "\nthe end " << end;
             length = 0;
             while (temporary->get_pointer() != nullptr){
                 temporary = temporary->get_pointer();
@@ -134,12 +114,10 @@ using namespace std;
             while (temporary_1 != nullptr){
                 temporary_1 = temporary_1->get_pointer();
                 linked_list_length++;
-              //  cout << " " << linked_list_length;
             }
             temporary_1 = head;
             if (position < 1 || position > linked_list_length)
                 return;
-         //   cout << "\n";
             if (linked_list_length < position || linked_list_length < 1)
                 cout << "outside range";
             if (position == 1){
@@ -161,10 +139,14 @@ using namespace std;
         int LinkedList::getItem(int position){
             Node* temporary = head;
             int size = 0;
+            if (position == 1){
+                cout << temporary->get_data() << " ";
+                return position;
+            }
             while (temporary->get_pointer() != nullptr){
                 temporary = temporary->get_pointer();
                 size++;
-                if (position == size){
+                if (position-1 == size){
                     cout << temporary->get_data() << " ";
                     return position;
                 }
@@ -174,10 +156,17 @@ using namespace std;
         }
         void LinkedList::printItems(){
             Node *temporary = head;
+            bool start = true;
             if (head == NULL)
                 cout << "there is no list\n";
             while (temporary != NULL){
+                if (start == true){
+                    cout << temporary->get_data();
+                    start = false;
+                }
+                else {
                 cout << " " << temporary->get_data();
+                }
                 temporary = temporary->get_pointer();
             }
         }
--- main.cpp
+++ main.cpp
@@ -1,39 +1,51 @@
 #include <iostream>
 #include <vector>
+#include <sstream>
 #include <limits>
 #include "Node.h"
 #include "LinkedList.h"
 using namespace std;
 
 int main(void){
-    // int array_integers[10] = {1,2,3,4,5,6,7,8,9,10};
-    // LinkedList the_second_list(array_integers,10);
-    vector<int> the_list;
-    string commands = "";
-    int num = 0;
-    while (true){
-        cin >> num;
-        the_list.push_back(num);
-        if (cin.peek() == '\n'){
-            break;
-        }
+    vector<string> String_Vector = {};
+    string input;
+    getline(cin, input);
+    stringstream stringggg_form;
+    stringggg_form << input;
+    string String_Temporary;
+    while(!stringggg_form.eof()) {
+        stringggg_form >> String_Temporary;
+        String_Vector.push_back(String_Temporary);
     }
-    int the_list_copy_array[the_list.size()];
-    for (unsigned int k = 0; k < the_list.size(); k++){
-        the_list_copy_array[k] = the_list.at(k);
+    int arr[100] = {};
+    int a = (String_Vector.size()-3);
+    for (int i = 0; i < a; i++) {
+        arr[i] = stoi(String_Vector[i]);
     }
-    LinkedList the_third_list(the_list_copy_array,the_list.size());
-    cout << "\n\n The items are: \n\n\n";
-    the_third_list.addAtPosition(3,9);
-  //  the_third_list.addEnd(9);
- //   the_third_list.addFront(9);
-    //  the_third_list.deleteFront();
-   // the_third_list.deleteEnd();
-   // the_third_list.deletePosition(6);
-    //the_third_list.deletePosition(3);
-    cout << "\n\n";
-    cout << " here is the numbers ";
-    the_third_list.printItems();
-    cout << "\n\n\n";
+    string str = String_Vector[String_Vector.size()-3];
+    int first_number = stoi(String_Vector[String_Vector.size()-2]);
+    int second_number = stoi(String_Vector[String_Vector.size()-1]);
+    LinkedList * LINKED_LIST = new LinkedList(arr, (String_Vector.size()-3));
+    if (str == "AF") {
+        LINKED_LIST->addFront(first_number);
+    } 
+    else if (str == "AE") {
+        LINKED_LIST->addEnd(first_number);
+    } 
+    else if (str == "AP") {
+        LINKED_LIST->addAtPosition(first_number, second_number);
+    } else if (str == "S") {
+        LINKED_LIST->search(first_number);
+    } else if (str == "DF") {
+        LINKED_LIST->deleteFront();
+    } else if (str == "DE") {
+        LINKED_LIST->deleteEnd();
+    } else if (str == "DP") {
+        LINKED_LIST->deletePosition(first_number);
+    } else if (str == "GI") {
+        LINKED_LIST->getItem(first_number);
+    } 
+    LINKED_LIST->printItems();
+    cout << "\n";
     return 0;
 }
