77f330b0fbcb8944d51f0c1c60d489bb92d68a99
Pahlavonjon <pahlavonjon.odilov@student.adelaide.edu.au>
2022-11-07 22:54:04 +1030

    added

--- practise.cpp
+++ practise.cpp
@@ -7,6 +7,7 @@
 #include <chrono>
 using namespace std;
 
+
 void print(bool* the_lock){
     while (true){
         if (*the_lock == true)
@@ -14,17 +15,40 @@ void print(bool* the_lock){
         this_thread::sleep_for(chrono::milliseconds(1250));
     }
 }
+// void test_function_(*functor_pointer)
+
+void the_running_thread(bool* lock_1);
+
+// void test_2(){
+//     bool lock_1 = true;
+//    // bool lock_2 = true;
+//   //  thread testing_thread(the_running_thread);
+//     the_running_thread(&lock_1);
+//     // thread first___thread(print,&lock_1);
+//     // first___thread.detach();
+//     while (true){
+//         while (lock_1 == false){
+//             cout << " \n Here the program is working\n\n";
+//             lock_1 = true;
+//         }
+//     }
+// }
+
 
 int main(void){
-    bool lock_1 = true;
-    bool lock_2 = true;
-    thread first___thread(print,&lock_1);
-    first___thread.detach();
-    while (true){
-        while (lock_1 == false){
-            cout << " \n Here the program is working\n\n";
-            lock_1 = true;
-        }
-    }
+    int* x,y;
+    x = new int;
+    *x = 25;
+    y = 15;
+    cout << " " << *x << " " << y << " \n\n";
+    y = *x;
+    cout << " " << *x << " " << y << " \n\n";
+
     return 0;
 }
+
+// void the_running_thread(bool* lock_1){
+//     thread first___thread(print,&lock_1);
+//     first___thread.detach();
+//    // this_thread::sleep_for(chrono::seconds(259));
+// }
